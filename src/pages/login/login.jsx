909028cf6d6bd2af3dce4a7d6bcdb2324877224a
ch <chenhua1920@163.com>
2020-04-29 17:07:58 +0800

    提交配置less

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import { Button } from 'antd';
+import './login.less'
+export default class Login extends Component {
+    render(){
+        return (
+            <div className="login">
+                <div className="loginheader">1</div>
+                <div className="logincontent">2</div>
+            </div>
+        )
+    }
+}
